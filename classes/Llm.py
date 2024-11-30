6393a456fc1e4a63c7a6545c503b0e604bcec57c
Joe Gilbert <joegilbertdev@outlook.com>
2024-11-30 18:04:07 +0000

    async llm call for each news article

--- classes/Llm.py
+++ classes/Llm.py
@@ -1,11 +1,13 @@
+import asyncio
 from tempfile import template
+from typing import List, Coroutine
 
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_ollama.llms import OllamaLLM
 
 from Prompts import SYSTEM_PROMPT
 from classes.CountryCodes import CountryCodes
-from classes.NewsRetriever import NewsRetriever
+from classes.NewsRetriever import NewsRetriever, NewsArticle
 
 
 class LLM:
@@ -13,31 +15,48 @@ class LLM:
         self.llm = OllamaLLM(model=model)
         self.country_code = country_code
 
-    def get_completion(self):
-        # Step 1: Get news data
-        retriever = NewsRetriever()
-        news_response = retriever.get_news(country_code=self.country_code)
+    async def _get_completions_async(self, articles: List[NewsArticle]) -> List[str]:
+        completions: List[Coroutine]= []
 
         # Step 2: Define a prompt
-        format_instructions = ""  # placeholder for pydantic
-        prompt = PromptTemplate(
+        for article in articles:
+            format_instructions = ""  # placeholder for pydantic
+            prompt = PromptTemplate(
             template="Answer the user query. \n {query}\n",
             input_variables=["query"],
             # partial_variables={"format_instructions": format_instructions},
         )
-        query_template = PromptTemplate.from_template(SYSTEM_PROMPT)
-        query = query_template.format(
-            articles=news_response[
-                0
-            ]  # TODO: return all articles and process each as a separate async LLM call
+            query_template = PromptTemplate.from_template(SYSTEM_PROMPT)
+            query = query_template.format(
+            articles=article
         )
 
-        # prompt = prompt_template.invoke({"news_articles": news_response})
+            # prompt = prompt_template.invoke({"news_articles": news_response})
 
-        # Step 3: Generate LLM response
-        # TODO: format as pydantic object
-        # TODO: to ensure that each news article is accurate send each as a single LLM prompt and response async
-        # completion: str = self.llm.invoke(prompt)
-        chain = prompt | self.llm  # parser
-        completion = chain.invoke(query)
-        return completion
+            # Step 3: Generate LLM responses
+            # TODO: format as pydantic object
+            # TODO: to ensure that each news article is accurate send each as a single LLM prompt and response async
+            # completion: str = self.llm.invoke(prompt)
+
+            chain = prompt | self.llm  # parser
+            completions.append(chain.ainvoke(query))
+
+        # Step 4: resolve coroutines and return
+        awaited_completions: List[str] = await asyncio.gather(*completions)
+        return awaited_completions
+
+
+
+    def get_completion(self):
+        # Step 1: Get news data
+        retriever = NewsRetriever()
+        news_responses = retriever.get_news(country_code=self.country_code)
+        sliced_news_responses = news_responses[:2]
+        print(sliced_news_responses)
+
+        completions = asyncio.run(self._get_completions_async(sliced_news_responses)) #todo: expand slice to more articles
+        return completions
+
+
+
+
--- classes/NewsRetriever.py
+++ classes/NewsRetriever.py
@@ -1,8 +1,7 @@
 import os
 import json
 from dataclasses import dataclass
-from typing import List, Dict
-
+from typing import List, Dict, Optional
 
 import requests
 from dotenv import load_dotenv
@@ -25,24 +24,24 @@ class NewsArticle:
     sentiment: float
     language: str
     source_country: str
+    category: Optional[str] = None
 
 
 class NewsRetriever:
     def _convert_articles_to_dataclass(self, article: Dict) -> NewsArticle:
         return NewsArticle(**article)
 
-    def get_news(self, country_code: CountryCodes) -> str:
+    def get_news(self, country_code: CountryCodes) -> List[NewsArticle]:
         load_dotenv()
         API_KEY = os.environ.get("API_KEY")
-        search_url = f"https://api.worldnewsapi.com/top-news?source-country={country_code.value}&language=en"
+        search_url = f"https://api.worldnewsapi.com/search-news?text=climate+change&source-country={country_code.value}&language=en" #todo: add a topic
         headers = {"x-api-key": API_KEY}
 
         response = requests.get(search_url, headers=headers)
         response.raise_for_status()
 
         search_results = json.loads(response.text)
-        print(search_results)
-        raw_articles = search_results["top_news"][0]["news"]
+        raw_articles = search_results["news"]
 
         converted_articles = [
             self._convert_articles_to_dataclass(article) for article in raw_articles
