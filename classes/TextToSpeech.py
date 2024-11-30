133cd1f7f8bdc6df29dc321cd7b7f77984b7d9cc
Joe Gilbert (He/Him) <104070284+joeglDev@users.noreply.github.com>
2024-11-30 22:15:41 +0000

    2: Add Text to Speech (#1)
        adds Get news/audio endpoint which returns LLM responses as a .wav audio file.
        Updated documentation

---
+++ classes/TextToSpeech.py
@@ -0,0 +1,58 @@
+import os
+
+import torch
+from TTS.api import TTS
+from playsound import playsound
+
+
+class TextToSpeech:
+    def __init__(self, tts_model: str, speaker: str, language: str):
+        self.tts_model = tts_model
+        self.speaker = speaker
+        self.language = language
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.tts = TTS(self.tts_model).to(self.device)
+        self.path = os.path.dirname(os.path.abspath(__file__)) + "/../data/output.wav"
+
+    def _synthesise_speech(self, completion: str):
+        # Run TTS
+        # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+        # Text to speech list of amplitude values as output
+        print("Generating voice...")
+
+        self.tts.tts_to_file(
+            text=completion,
+            file_path=self.path,
+            speaker=self.speaker,
+            language=self.language,
+            split_sentences=True,
+        )
+
+    def _speak(self):
+        """play audio from file"""
+
+        print("Speaking...")
+        playsound(self.path)
+
+    def _clean_file(self):
+        try:
+            os.remove(self.path)
+        except Exception:
+            print("No preexisting audio file file to delete")
+
+    def _read_file(self) -> bytes:
+        chunk_size = 4096
+        try:
+            with open(self.path, "rb") as file:
+                while True:
+                    chunk = file.read(chunk_size)
+                    if not chunk:
+                        break
+                    yield chunk
+        except IOError as e:
+            print(f"Error reading audio file: {e}")
+
+    def run(self, completion: str):
+        self._clean_file()
+        self._synthesise_speech(completion)
+        return self._read_file()
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 
 from classes.CountryCodes import CountryCodes
 from classes.Llm import LLM
+from classes.TextToSpeech import TextToSpeech
 
 
 @dataclass
@@ -20,8 +23,29 @@ def ping():
 
 
 @app.get("/news")
-def get_news(country_code: CountryCodes, topic: str):
+def get_news(country_code: CountryCodes, topic: str) -> GetNewsResponse:
     llm = LLM(model="mistral", country_code=country_code, topic=topic)
     completion = llm.get_completion()
     response = GetNewsResponse(text_summary=completion)
+    return response
+
+
+@app.get("/news/audio")
+def get_news(country_code: CountryCodes, topic: str) -> StreamingResponse:
+    llm_service = LLM(model="mistral", country_code=country_code, topic=topic)
+    completions = llm_service.get_completion()
+    completions_str = "Next article: ".join(completions)
+
+    tts_service = TextToSpeech(
+        tts_model="tts_models/multilingual/multi-dataset/xtts_v2",
+        speaker="Baldur Sanjin",
+        language="en",
+    )
+    audio_stream = tts_service.run(completions_str)
+    file_name = f"{datetime.today().strftime('%Y-%m-%d')}-news"
+    headers = {"Content-Disposition": f'attachment; filename="{file_name}.wav"'}
+
+    response = StreamingResponse(
+        content=audio_stream, media_type="audio/wav", headers=headers
+    )
     return response
